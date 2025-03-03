87420ea3aef6acc75db6e44f50756ff9cbd2b4fa
Matt M <matte773@gmail.com>
2025-03-03 15:03:38 -0800

    prestudy push

--- build_run.sh
+++ build_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Building
-sudo docker build -t quori-ros-image --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_ed25519)" --build-arg ROBOT_IP=$(cat robot_ip.txt) --build-arg GUI_IP=$(cat gui_ip.txt) .
+sudo docker build --no-cache -t quori-ros-image --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_ed25519)" --build-arg ROBOT_IP=$(cat robot_ip.txt) --build-arg GUI_IP=$(cat gui_ip.txt) .
 
 # Running 
 sudo docker run -it --rm -e DISPLAY=$DISPLAY --privileged --network host -v /dev:/dev -v /tmp/.X11-unix:/tmp/.X11-unix quori-ros-image
